899de60f2c8db591b19eda80602755b373f3040e
Mike McDonald <mike.mcdonald@continuum.net>
2014-08-19 16:01:37 -0400

    Added basic controllers and views

---
+++ src/WebDemos/Controllers/FileUploadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDemos.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        //
+        // GET: /FileUpload/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
